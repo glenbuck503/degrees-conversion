41b811561c63a42405f90ce37f9f16265e14bc51
Bess Campbell <bess.k.campbell@gmail.com>
2020-10-19 14:51:25 -0700

    Add jquery-3.5.1.js
    Co-authored-by: Glen Buck <glenbuck503@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function convert(celsius) {
+  return fahrenheit
+  }
+  
+  const celsius=parseInt(prompt("Enter the degrees in Celsius:"));
+  const fahrenheit= celsius*9/5+32;
+  alert(convert(celsius))
+  
